c38ab4da42cd733e5be3b9b8929f2ef691fbcbe4
Haris Shaikh <79969366+compilerharris@users.noreply.github.com>
2025-03-14 12:05:28 +0530

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -95,7 +95,7 @@ export default function Home() {
         <meta name="description" content="This is a demo for visualizer." />
         <meta property="og:title" content="Visualizer Demo | HardReload" />
         <meta property="og:description" content="This is a demo for visualizer." />
-        <meta property="og:image" content="https://netbizlabs.com/a+content/image1.jpg" />
+        <meta property="og:image" content="https://searchmaar.com/uploads/cdn/Eid-2025-SM.gif" />
         {/* <meta property="og:image" content="https://media.licdn.com/dms/image/v2/D4D0BAQG7g6qdNynOwg/company-logo_100_100/company-logo_100_100/0/1724951451490/hardreload_logo?e=1748476800&v=beta&t=tYaVxlyeFRv7af27GZjKKiZp8VoYgRK6YH1BNmk75Ww" /> */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://visualizer-demo.vercel.app" />
@@ -104,7 +104,7 @@ export default function Home() {
         <meta name="twitter:card" content="SMart Card" />
         <meta name="twitter:title" content="Visualizer Demo | HardReload" />
         <meta name="twitter:description" content="This is a demo for visualizer." />
-        <meta name="twitter:image" content="https://netbizlabs.com/a+content/image1.jpg" />
+        <meta name="twitter:image" content="https://searchmaar.com/uploads/cdn/Eid-2025-SM.gif" />
         {/* <meta name="twitter:image" content="https://media.licdn.com/dms/image/v2/D4D0BAQG7g6qdNynOwg/company-logo_100_100/company-logo_100_100/0/1724951451490/hardreload_logo?e=1748476800&v=beta&t=tYaVxlyeFRv7af27GZjKKiZp8VoYgRK6YH1BNmk75Ww" /> */}
       </Head>
       <h1>Home Glazer - Visualiser Demo</h1>
